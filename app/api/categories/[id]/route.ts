b176607d9d66b43640c3b0dc1f77295a44f44d47
Steve Rayner <srayner02@gmail.com>
2024-12-11 21:34:33 +0000

    Added categories prisma schema and api endpoints

---
+++ app/api/categories/[id]/route.ts
@@ -0,0 +1,77 @@
+import prisma from "@/app/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const category = await prisma.category.findUnique({
+      where: { id: params.id },
+    });
+
+    if (!category) {
+      return NextResponse.json(
+        { error: "Category not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(category, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const data = await request.json();
+    delete data.id;
+
+    const updatedCategory = await prisma.category.update({
+      where: { id: params.id },
+      data: data,
+    });
+
+    return NextResponse.json(updatedCategory, { status: 200 });
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return NextResponse.json(
+        { error: "Category not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const deletedCategory = await prisma.category.delete({
+      where: { id: params.id },
+    });
+
+    return NextResponse.json(deletedCategory, { status: 200 });
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return NextResponse.json(
+        { error: "Category not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
---
+++ prisma/migrations/20241211204601_add_category/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(64) NULL,
+    `parent_id` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Category_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
