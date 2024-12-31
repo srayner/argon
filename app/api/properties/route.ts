59bf5ca8caecef366a4d1900d3fc7324c6ee6674
Steve Rayner <srayner02@gmail.com>
2024-12-31 10:39:53 +0000

    added properties api routes for post and list

---
+++ app/api/properties/route.ts
@@ -0,0 +1,83 @@
+import prisma from "@/app/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { NextResponse, NextRequest } from "next/server";
+import createSearchObject from "../functions/create-search-object";
+
+export async function GET(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get("search") || "";
+    const searchFields = ["name"];
+    const searchObject = createSearchObject(searchFields, searchTerm);
+    const page = parseInt(url.searchParams.get("page") || "1", 10);
+    const pageSize = parseInt(url.searchParams.get("pageSize") || "10", 10);
+
+    if (page < 1 || pageSize < 1) {
+      return NextResponse.json(
+        { error: "Invalid pagination parameters" },
+        { status: 400 }
+      );
+    }
+
+    const totalItems = await prisma.property.count({ where: searchObject });
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    if (page > 1 && page > totalPages) {
+      return NextResponse.json({ error: "Page not found" }, { status: 404 });
+    }
+
+    const properties = await prisma.property.findMany({
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      where: searchObject,
+    });
+
+    return NextResponse.json({
+      data: properties,
+      meta: {
+        totalItems,
+        totalPages,
+        currentPage: page,
+        pageSize,
+      },
+    });
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { error: "A database error occurred" },
+        { status: 500 }
+      );
+    }
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+    const createdProperty = await prisma.property.create({ data: data });
+
+    return NextResponse.json(createdProperty, { status: 201 });
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      switch (error.code) {
+        case "P2002":
+          return NextResponse.json(
+            { error: "A property with this unique field already exists" },
+            { status: 409 }
+          );
+        case "P2003":
+          return NextResponse.json(
+            { error: "Referenced foreign key not found" },
+            { status: 400 }
+          );
+        default:
+          return NextResponse.json(
+            { error: "A database error occurred" },
+            { status: 500 }
+          );
+      }
+    }
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
