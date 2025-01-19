763db891910fd0c209d9b2454cd5a65b659160cb
Steve Rayner <srayner02@gmail.com>
2025-01-19 13:52:22 +0000

    added aggregated property values to properties endpoint

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,14 +1,12 @@
 import prisma from "@/app/prisma";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextResponse, NextRequest } from "next/server";
-import createSearchObject from "../functions/create-search-object";
 
 export async function GET(request: NextRequest) {
   try {
     const url = new URL(request.url);
-    const searchTerm = url.searchParams.get("search") || "";
-    const searchFields = ["name"];
-    const searchObject = createSearchObject(searchFields, searchTerm);
+    const categoryId = url.searchParams.get("categoryId") || "";
+    const searchObject = { categoryId: categoryId };
     const page = parseInt(url.searchParams.get("page") || "1", 10);
     const pageSize = parseInt(url.searchParams.get("pageSize") || "10", 10);
 
@@ -30,10 +28,54 @@ export async function GET(request: NextRequest) {
       skip: (page - 1) * pageSize,
       take: pageSize,
       where: searchObject,
+      include: {
+        propertyValues: {
+          select: {
+            valueString: true,
+            valueNumeric: true,
+          },
+        },
+      },
+    });
+
+    // Aggregate the property value counts
+    const aggregatedProperties = properties.map((property) => {
+      // Decide which value to use based on the property type
+      const useNumeric = property.type === "NUMERIC";
+
+      // Initialize an array to hold the aggregated results
+      const aggregatedValues: Array<{ value: any; count: number }> = [];
+
+      property.propertyValues.forEach((pv) => {
+        // Choose the value based on the property type
+        const value = useNumeric ? pv.valueNumeric : pv.valueString;
+
+        // If value is valid, proceed with counting
+        if (value !== null && value !== undefined) {
+          // Check if the value already exists in the aggregated array
+          const existingEntry = aggregatedValues.find(
+            (entry) => entry.value === value
+          );
+
+          if (existingEntry) {
+            // If it exists, increment the count
+            existingEntry.count++;
+          } else {
+            // If it doesn't exist, add it with a count of 1
+            aggregatedValues.push({ value, count: 1 });
+          }
+        }
+      });
+
+      // Return the property with aggregated property values
+      return {
+        ...property,
+        propertyValues: aggregatedValues,
+      };
     });
 
     return NextResponse.json({
-      data: properties,
+      data: aggregatedProperties,
       meta: {
         totalItems,
         totalPages,
