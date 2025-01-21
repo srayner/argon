a7be5981503dd663d5eea0c4dc9d499bcdbf530e
Steve Rayner <srayner02@gmail.com>
2025-01-21 08:35:04 +0000

    listboxes now locally sort their own options

--- components/ui/listbox.tsx
+++ components/ui/listbox.tsx
@@ -28,12 +28,30 @@ const ListBox: React.FC<ListBoxProps> = ({
 
   const widthClass = width ? width : "w-full";
 
+  function sort(array: ListBoxOption[]): ListBoxOption[] {
+    array.sort((a, b) => {
+      if (typeof a.value === "number" && typeof b.value === "number") {
+        return a.value - b.value;
+      }
+
+      if (typeof a.value === "string" && typeof b.value === "string") {
+        return a.value.localeCompare(b.value);
+      }
+
+      return a.name.localeCompare(b.name);
+    });
+
+    return array;
+  }
+
+  const sortedOptions = sort(options);
+
   return (
     <div className={`mb-4 ${widthClass}`}>
       <h3 className="font-bold text-sm mb-2 px-2">{label}</h3>
       <div className="bg-white rounded p-2 w-full h-[176px] overflow-y-auto">
         <ul>
-          {options.map((option) => (
+          {sortedOptions.map((option) => (
             <li
               key={option.value}
               className={`p-1 text-sm leading-none cursor-pointer hover:text-blue-500 ${
