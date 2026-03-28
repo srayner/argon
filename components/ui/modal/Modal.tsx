da471ee81f41b13b8954c6076bded84307e53c25
Steve Rayner <srayner02@gmail..com>
2026-03-28 21:15:04 +0000

    improved modal styles and close behaviour

--- components/ui/modal/Modal.tsx
+++ components/ui/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import { HiX } from "react-icons/hi";
 
 interface ModalProps {
   isVisible: boolean;
@@ -16,17 +17,28 @@ const Modal: React.FC<ModalProps> = ({
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-[99]">
-      <div className="bg-white p-5 rounded-md flex flex-col items-center gap-5 min-h-[150px] min-w-[400px] relative z-[100]">
+    <div
+      className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-[99]"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white p-10 pb-5 rounded-md flex flex-col items-center gap-5 min-h-[150px] min-w-[400px] relative z-[100]"
+        onClick={(e) => e.stopPropagation()}
+      >
         {showCloseCross && (
           <button
-            className="absolute top-[10px] right-[10px] bg-none border-none text-[20px] cursor-pointer text-[#333]"
+            className="
+                absolute top-[10px] right-[10px] p-1
+                bg-none text-[20px] cursor-pointer text-[#333]
+                transition-all duration-200
+                hover:bg-gray-200 hover:rounded
+            "
             onClick={onClose}
           >
-            X
+            <HiX size={20} />
           </button>
         )}
-        {children}
+          {children}
       </div>
     </div>
   );
